96340dbbc68803c8313fc27e3ccdc601bab170a2
unknown <you@example.com>
2024-01-24 16:59:22 +0800

    created loop to find the numbers, display the results

--- numbers_divisible_by_5.py
+++ numbers_divisible_by_5.py
@@ -5,5 +5,9 @@ number_list = [10, 20, 33, 46, 55]
 print(number_list)
 
 # Find the numbers divisible by 5
+print("Numbers divisible by 5")
+for num in number_list:
+    if num % 5 == 0:
 
 # Show the numbers divisible by 5
+        print(num)
